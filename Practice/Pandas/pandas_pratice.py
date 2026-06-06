ff87ad2ad6b99079f09b9fd91f1368b3aca1f59d
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-06 17:22:38 +0500

    added the code

--- Practice/Pandas/pandas_pratice.py
+++ Practice/Pandas/pandas_pratice.py
@@ -7,3 +7,28 @@ print(covid_df.info())
 print(covid_df.describe())
 print(covid_df.columns)
 print(covid_df.shape)
+
+print(covid_df['new_cases']) 
+
+print(type(covid_df['new_cases'])) 
+
+print(covid_df['new_cases'][246])  
+
+print(covid_df.at[241, 'new_cases'])  
+
+print(covid_df.new_cases) 
+
+print(covid_df[['new_cases', 'date']])  
+
+subset = covid_df[['new_cases', 'date']].copy()
+
+print(covid_df.loc[243])  
+
+print(covid_df.head(5))  
+print(covid_df.tail(5))  
+
+print(covid_df.loc[108:113])  
+
+print(covid_df.new_tests.first_valid_index())  
+
+print(covid_df.sample(10)) 
