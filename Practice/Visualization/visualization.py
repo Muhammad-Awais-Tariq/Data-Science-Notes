8feef3d2badc86782acd91ebb33967d8c4781b90
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-06-15 14:54:32 +0500

    added the required code

--- Practice/Visualization/visualization.py
+++ Practice/Visualization/visualization.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+sns.set_style("whitegrid")
+
 yield_apples = [0.895 , 0.91 , 0.919 , 0.926 , 0.929 , 0.931]
 yield_oranges = [0.925, 0.921, 0.900, 0.895, 0.890, 0.885]
 years = [2010, 2011 , 2012 , 2013 , 2014 , 2016]
